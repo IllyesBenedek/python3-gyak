7c58e2ff9253c8f6c4a657a0a91afe5dbd86d1d1
Illyés Benedek <benedekillyes@gmail.com>
2026-03-26 19:53:28 +0100

    Add files via upload

---
+++ python3/python3.py
@@ -0,0 +1,360 @@
+#--------------------------
+'''
+A leggyakoribb_karakter(fname) függvény
+paraméterként egy fájlnevet kap és
+visszatér a  fájlban leggyakrabban előforduló karakterrel.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Páros számok száma egy szövegfájlban.
+Írj egy függvényt paros_szamok_szama_a_fajlban néven, amely visszatér egy szövegfájlban levő páros számok számával.
+A függvény bemenő paramétere a fájl neve.
+'''
+def paros_szamok_szama_a_fajlban(fajnev):
+    f = open(fajnev, "r")
+    szamok = f.read().split()
+    f.close()
+    par = 0
+    for i in szamok:
+        if int(i) % 2 == 0:
+            par += 1
+    return par
+
+#--------------------------
+'''
+A leghosszabb_sor_hossza(fname) függvény
+paraméterként egy fájlnevet kap és
+visszatér a  fájlban levő leghosszabb sor hosszával.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Pozitív számok száma egy szövegfájlban.
+Írj egy függvényt pozitiv_szamok_szama_a_fajlban néven, amely visszatér egy szövegfájlban levő pozitiv számok számával.
+A függvény bemenő paramétere a fájl neve.
+'''
+def pozitiv_szamok_szama_a_fajlban(fajnev):
+    f = open(fajnev, "r")
+    szamok = f.read().split()
+    poz = 0
+    for i in szamok:
+        if int(i) > 0:
+            poz += 1
+    return poz
+
+
+#--------------------------
+'''
+Feladat: Páratlanok egy szövegfájlból.
+Írj egy függvényt paratlanok_a_fajlbol néven, amely visszatér a szövegfájlban levő páratlan számokkal mint listával.
+A függvény bemenő paramétere a fájl neve.
+'''
+def paratlanok_a_fajlbol(fajnev):
+    f = open(fajnev, "r")
+    szamok = f.read().split()
+    f.close()
+    parat = []
+    for i in szamok:
+        if int(i) % 2 != 0:
+            parat.append(int(i))
+    return parat
+
+
+#--------------------------
+'''
+Feladat: Páratlan számok száma egy szövegfájlban.
+Írj egy függvényt paratlan_szamok_szama_a_fajlban néven, amely visszatér egy szövegfájlban levő páratlan számok számával.
+A függvény bemenő paramétere a fájl neve.
+'''
+def paratlan_szamok_szama_a_fajlban(fajnev):
+    f = open(fajnev, "r")
+    szamok = f.read().split()
+    parat = 0
+    for i in szamok:
+        if int(i) % 2 != 0:
+            parat += 1
+    return parat
+
+
+#--------------------------
+'''
+Feladat: Pozitívok egy szövegfájlból.
+Írj egy függvényt pozitiv_a_fajlbol néven, amely visszatér a szövegfájlban levő pozitiv számokkal mint listával.
+A függvény bemenő paramétere a fájl neve.
+'''
+def pozitiv_a_fajlbol(fajnev):
+    f = open(fajnev, "r")
+    szamok = f.read().split()
+    f.close()
+    poz = []
+    for i in szamok:
+        if int(i) > 0:
+            poz.append(int(i))
+    return poz
+
+
+#--------------------------
+'''
+Feladat: Neggyel osztható számok a szövegfájlban.
+A neggyel_oszthato_szamok_a_fajlban függvény
+egy függvényt neggyel_oszthato_szamok_a_fajlban néven, amely visszatér a szövegfájlban levő neggyel osztható számok listájával.
+A függvény bemenő paramétere a fájl neve.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Negatív számok száma egy szövegfájlban.
+Írj egy függvényt negativ_szamok_szama_a_fajlban néven, amely visszatér egy szövegfájlban levő negativ számok számával.
+A függvény bemenő paramétere a fájl neve.
+'''
+def negativ_szamok_szama_a_fajlban(fajnev):
+    f = open(fajnev, "r")
+    szamok = f.read().split()
+    f.close()
+    neg = 0
+    for i in szamok:
+        if int(i) < 0:
+            neg += 1
+    return neg
+
+
+#--------------------------
+'''
+Feladat: Hárommal osztható számok a szövegfájlban.
+Írj egy függvényt harommal_oszthato_szamok_a_fajlban néven, amely visszatér a szövegfájlban levő hárommal osztható számok listájával.
+A függvény bemenő paramétere a fájl neve.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Számtani sorozat fájlba írása
+Készíts függvényt szaz_szam_fajlba néven, amely 1-tól 100-ig egyesével kiírja a számokat egy fájlba.
+Minden szám kerüljön új sorba.
+A fájl nevét paraméterként kapja meg a függvény.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Párosok egy szövegfájlból.
+Írj egy függvényt parosok_a_fajlbol néven, amely visszatér a szövegfájlban levő páros számokkal mint listával.
+A függvény bemenő paramétere a fájl neve.
+'''
+def parosok_a_fajlbol(fajnev):
+    f = open(fajnev, "r")
+    szamok = f.read().split()
+    f.close()
+    par = []
+    for i in szamok:
+        if int(i) % 2 == 0:
+            par.append(int(i))
+    return par
+
+
+#--------------------------
+'''
+Feladat: Négyzet osztály definiálása. 
+[Objektumorientált programozás]
+Hozz létre egy osztályt Negyzet néven.
+A Negyzet osztály lehetővé teszi a negyzet oldalhosszúságának tárolását.
+A Negyzet osztály rendelkezik egy kerulet() nevü metódussal, 
+    amely az osztály segítségével létrehozott objektum metódusaként 
+        visszaadja az adott objektum kerületét.
+A Negyzet osztály rendelkezik egy terulet() nevü metódussal, 
+    amely az osztály segítségével létrehozott objektum metódusaként 
+        visszaadja az adott objektum területét.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: String fájlba írása
+A string_fajlba nevű függvény az első paraméterként kapott sztringet fájlba írja.
+A fájl nevét második paraméterként kapja meg a függvény.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Legnagyobb szám egy szövegfájlban.
+Írj egy függvényt legnagyobb_szam_a_fajlban néven, amely visszatér egy szövegfájlban levő legnagyobb számmal.
+A függvény bemenő paramétere a fájl neve.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Utolsó karakter a szövegfájlban
+Írj egy függvényt utolso_karakter_a_fajlban néven, amely visszatér egy szövegfájl utolsó karakterével.
+A függvény bemenő paramétere a fájl neve.
+'''
+def utolso_karakter_a_fajlban(fajnev):
+    f = open(fajnev, "r")
+    szoveg = f.read()
+    f.close()
+    if szoveg == "":
+        return None
+    return szoveg[-1]
+
+
+#--------------------------
+'''
+Feladat: Kocka osztály definiálása. 
+[Objektumorientált programozás]
+Hozz létre egy osztályt Kocka néven.
+A Kocka osztály lehetővé teszi a kocka oldalhosszúságának tárolását.
+A Kocka osztály rendelkezik egy tefogat() nevü metódussal, 
+    amely az osztály segítségével létrehozott objektum metódusaként 
+        visszaadja az adott objektum térfogatát.
+A Kocka osztály rendelkezik egy felszin() nevü metódussal, 
+    amely az osztály segítségével létrehozott objektum metódusaként 
+    visszaadja az adott objektum felszínét.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Leggyakoribb szám a szövegfájlban.
+Írj egy függvényt leggyakoribb_szam_a_fajlban néven, amely visszatér a szövegfájlban levő leggyakoribb számmal.
+A függvény bemenő paramétere a fájl neve.
+'''
+
+
+
+#--------------------------
+'''
+A karakterek_szama nevű függvény
+paraméterként egy fájlnevet kap és
+visszatér a fájlban levő karakterek számával. 
+('\n karakterekkel együtt')
+'''
+
+
+
+#--------------------------
+'''
+A lorem_szavak_szama nevű függvény 
+paraméterként egy fájlnevet kap és
+visszatér a  fájlban levő "lorem" szavak számával.
+'''
+
+
+
+#--------------------------
+'''
+Az r_betuk_szama nevű függvény
+paraméterként egy fájlnevet kap és
+visszatér a fájlban levő 'r' betük számával.
+'''
+
+
+
+#--------------------------
+'''
+A sorok_szama nevű függvény
+paraméterként egy fájlnevet kap és
+visszatér a fájlban levő sorok számával.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Legkisebb szám egy szövegfájlban.
+Írj egy függvényt legkisebb_szam_a_fajlban néven, amely visszatér egy szövegfájlban levő lekisebb számmal.
+A függvény bemenő paramétere a fájl neve.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Számok átlaga egy szövegfájlban.
+Írj egy függvényt szamok_atlaga_a_fajlban néven, amely visszatér egy szövegfájlban levő számok átlagával.
+A függvény bemenő paramétere a fájl neve.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Negatívok egy szövegfájlból.
+Írj egy függvényt negativok_a_fajlbol néven, amely visszatér a szövegfájlban levő negativ számokkal mint listával.
+A függvény bemenő paramétere a fájl neve.
+'''
+def negativok_a_fajlbol(fajnev):
+    f = open(fajnev, "r")
+    szamok = f.read().split()
+    f.close()
+    neg = []
+    for i in szamok:
+        if int(i) < 0:
+            neg.append(int(i))
+    return neg
+
+
+#--------------------------
+'''
+A szavak_szama nevű függvény
+paraméterként egy fájlnevet kap és
+visszatér a fájlban levő szavak számával.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Első karakter a szövegfájlban
+Írj egy függvényt elso_karakter_a_fajlban néven, amely visszatér egy szövegfájl első karakterével.
+A függvény bemenő paramétere a fájl neve.
+'''
+def elso_karakter_a_fajlban(fajnev):
+    f = open(fajnev, "r")
+    szoveg = f.read()
+    f.close()
+    if szoveg == "":
+        return None
+    return szoveg[0]
+    
+
+
+#--------------------------
+'''
+Feladat: Számok összege egy szövegfájlban.
+Írj egy függvényt szamok_osszege_a_fajlban néven amely visszatér egy szövegfájlban levő számok összegével.
+A függvény bemenő paramétere a fájl neve.
+'''
+
+
+
+#--------------------------
+'''
+Feladat: Téglalap osztály definiálása. 
+[Objektumorientált programozás]
+Hozz létre egy osztályt Teglalap néven.
+A Teglalap osztály lehetővé teszi a téglalap oldalhosszúságainak tárolását.
+A Teglalap osztály rendelkezik egy kerulet() nevü metódussal, 
+    amely az osztály segítségével létrehozott objektum metódusaként 
+        visszaadja az adott objektum kerületét.
+A Teglalap osztály rendelkezik egy terulet() nevü metódussal, 
+    amely az osztály segítségével létrehozott objektum metódusaként 
+        visszaadja az adott objektum területét.
+'''
+
+
+
+#======================================================================================================================C:\Users\bened\python3
